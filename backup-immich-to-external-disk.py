ca9297351da60c39f40356b341e0d49ed6ae9a73
Henry Post <henryfbp@gmail.com>
2026-01-29 20:32:04 -0600

    add instr for backup

--- backup-immich-to-external-disk.py
+++ backup-immich-to-external-disk.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
 
+'''
 1. see if we're mounted
 2. list existing backups, show space stats w/ a cool graphic
 3. ask if we want to delete the latest if we have >7 backups
 4. use rsync to back up the relevant docker compose volumes
 5. show stats of our most recent backup
+'''
